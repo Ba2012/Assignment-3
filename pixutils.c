9b22022d8e9768efb5d46fc47bc3ed24e439a8cd
Ba2012 <ba2012@uw.edu>
2016-08-09 17:26:49 -0700

    ASSIGNMENT4

--- pixutils.c
+++ pixutils.c
@@ -1,94 +1,212 @@
 #include "pixutils.h"
 #include "lodepng.h"
+#include "bmp.h"
 
 //private methods
-static pixMap *pixMap_init(); //allocate memory for pixMap object, set variables to zero, and return a pointer to the object
+static pixMap *pixMap_init(); //allocate memory for pixMap and set variables to zero
 static void pixMap_reset();  //free the allocated memoray and set to zero but do not free memory for pixMap
-static void pixMap_copy(pixMap *dest,pixMap *source); //copy one pixmap to another
-static int pixMap_read(pixMap *p,char *filename); //read in a pixmap from file
+static void pixMap_copy(pixMap *dest,pixMap *source);
+static int pixMap_read(pixMap *p,char *filename);
 
 static pixMap* pixMap_init(){
- //allocate memory for pixMap object, set variables to zero, and return a pointer to the object
+	pixMap *p;
+	if(!(p=malloc(sizeof(pixMap))))exit(EXIT_FAILURE);
+	p->image=0;
+	p->pixArray=0;
+	p->width=0;
+	p->height=0;
+	return p;
 }	
 pixMap* pixMap_init_filename(char *filename){
-	//use pixMap_int to create the pixMap object
-	//use pixMap read to fill the fields of the object
-	//return the pointer to the new object
+	pixMap *p=pixMap_init();
+ pixMap_read(p,filename);
+ if(!(p->pixArray=malloc(p->height*sizeof(rgba*))))exit(EXIT_FAILURE);
+ p->pixArray[0]=(rgba*)p->image;
+ for (int i=1;i<p->height;i++)
+  p->pixArray[i]=p->pixArray[i-1]+p->width;
+ return p;
 }
 static int pixMap_read(pixMap *p,char *filename){
- //read and allocate image, read in width and height using using lodepng_decode32_file
- //example is in lodepng/examples - one liner
-	//then allocate p->pixArray to hold p->height pointers
-	//set p->pixArray[0] to p->image and p->pixArray[i]=p->pixArray[i-1]+p->width
+	int error;
+ if((error=lodepng_decode32_file(&(p->image), &(p->width), &(p->height),filename))){
+  fprintf(stderr,"error %u: %s\n", error, lodepng_error_text(error));
+  return 1;
+	}
 	return 0;
 }	
 static void pixMap_copy(pixMap *dest,pixMap *source){
 	//if source image is zero then reset dest and copy width and height
-	//if source image is not zero
-	  //if width or height are different
-	    //if width*height is different then we need to allocate dest->image
-	      //if dest->image is zero use malloc to allocate memory for dest->image
-	      //else use realloc to allocate memory dest->image
+	//if source image is not zero 
+	  //if width or height are different 
+	    //if width*height is different
+	      //if dest->image is zero use malloc for dest->image
+	      //else use realloc for dest->image
+	      //if dest->height is different malloc or realloc dest->pixArray depending on whether it is zero 
+	      //set dest->pixArray[0] to dest->image and dest->pixArray[i]=dest->pixArray[i-1]+source->width
 	    //
-	    //if dest->height is different
-	      //malloc or realloc dest->pixArray depending on whether dest->pixArray is zero
-	    //
-	    //even if the height is the same set dest->pixArray[0] to dest->image and dest->pixArray[i]=dest->pixArray[i-1]+source->width 
 	  //
-	  //do a memcpy from source->image to dest->image
-	  //set dest->width and dest->height to source values
+	 //do a memcpy from source to dest
+	 //set dest->width and dest->height to source values
 	//
+ if(!source->image){
+	 pixMap_reset(dest);
+	 dest->width=source->width;
+	 dest->height=source->height;
+	}
+	if(dest->width != source->width || dest->height != source->height){
+		if(dest->width * dest->height != source->width * source->height){
+			if(dest->image){
+				if(!(dest->image=realloc(dest->image,source->height*source->width*sizeof(rgba))))exit(EXIT_FAILURE);
+			}
+			else if(!(dest->image=malloc(source->height*source->width*sizeof(rgba))))exit(EXIT_FAILURE);
+		}
+	 if(dest->height != source->height){
+	  if(dest->pixArray){
+	   if(!(dest->pixArray=realloc(dest->pixArray,source->height*sizeof(rgba*))))exit(EXIT_FAILURE);
+			}
+			else if(!(dest->pixArray=malloc(source->height*sizeof(rgba*))))exit(EXIT_FAILURE);
+		}
+		dest->pixArray[0]=(rgba*)dest->image;
+		for(int i=1;i<source->height;i++){
+			dest->pixArray[i]=dest->pixArray[i-1]+source->width;
+		}	
+	}		
+	memcpy(dest->image,source->image,source->height*source->width*sizeof(rgba));
+ dest->height=source->height;
+ dest->width=source->width;
 }
 
 static void pixMap_reset(pixMap *p){
-	//free all the memory in the fields and set everything to zero
+	if (p && p->image){
+		free(p->image);
+	 p->image=0;
+	} 
+ if (p && p->pixArray){
+		free(p->pixArray);
+		p->pixArray=0;
+	}
 }	
 
 
 void pixMap_destroy(pixMap *p){
-	//reset pixMap and free the object p
+ if(p) pixMap_reset(p);
+ if(p) free(p);
 }
 
 
 void pixMap_rotate (pixMap *p, float theta){
- //make a new temp blank pixMap structure
- //copy p to temp
- //set the values in the image to zero using memset - Note that the 3rd argument of memset is the size in BYTES
-
- //calculate the coordinates ox and oy of the middle of the png graphic
- //calculate the values of sine and cosine used by the rotation formula 
-
-	//for(int y=0;y<p->height;y++){   //two for loops to loop through each pixel in the original
-	 //for(int x=0;x<p->width;x++){
-	 
-	    //calculate the new rotated coordinates rotx roty using the formula from 
+	//correct rotation that uses inverse rotation to determine which pixels to copy from the original
+	//the previous method would leave holes in the image
+	
+	pixMap *temp=pixMap_init();//make a new temp blank pixMap structure
+	pixMap_copy(temp,p); //copy the p to temp
+	//set the values in the image to zero using memset - that the 3rd argument of memset is the size in BYTES
+ memset(temp->image,0,temp->width*temp->height*sizeof(rgba));
+ 
+ const float ox=p->width/2.0f;
+ const float oy=p->height/2.0f;
+ const float s=sin(degreesToRadians(-theta));
+	const float c=cos(degreesToRadians(-theta));
+	
+	//for()   //double for loop to loop through each pixel in the original
+	  //for()
+	    //calculate the old coordinates rotx roty to copy from using the formula from 
 	    //http://stackoverflow.com/questions/2259476/rotating-a-point-about-another-point-2d
 	    //use the answer from stackoverflowery
- 	   //round the coordinates to the nearest integer in your calculations (add 0.5 and cast to integer)	
-	
-	    //if rotated coordinates are within the height and width limits
-	      //copy the pixel at the old coords to the pixel to the temporary copy using memcpy
-	      //i.e. 	memcpy(temp->pixArray[roty]+rotx,p->pixArray[y]+x,sizeof(rgba))
-	    //
 	    
+ 	   //round the coordinates to the nearest integer in your calculations (add 0.5 and cast to integer)	
+	    //if rotated coordinates are still inside the image
+	      //copy the pixel at the old coords to the pixel to the temporary copy using memcpy
+	      //i.e. 	memcpy(temp->pixArray[y]+x,p->pixArray[roty]+rotx,sizeof(rgba))
+	    //
 	  //
 	//  
 	//copy the temp pixMap to the original
-	//destroy the temp;
-}	
+	//destroy the temp
 
+	for(int y=0;y<p->height;y++){
+		for(int x=0;x<p->width;x++){ 
+   float rotx = c*(x-ox) - s * (oy-y) + ox;
+   float roty = -(s*(x-ox) + c * (oy-y) - oy);
+		 int rotj=rotx+.5;
+		 int roti=roty+.5; 
+		 if(roti >=0 && roti < temp->height && rotj >=0 && rotj < temp->width){
+				memcpy(temp->pixArray[y]+x,p->pixArray[roti]+rotj,sizeof(rgba));
+			}	
+		}	
+	}
+	pixMap_copy(p,temp);
+	pixMap_destroy(temp);
+	return;
+}	
 void pixMap_gray (pixMap *p){
-	//for() loop through pixels using two for loops 
-	  //for()
-	    //calculate average value of r and g and b values (assign to a float variable)
-	    //for example the red value of for the pixel at height i, width j would be p->pixel[i][j].r
-	    //round float variable to integer (add 0.5 before casting to integer)
-	    //assign the rounded value to r,g and b values
-	  //
-	//   
+
+	//loop through pixels using double for loop
+	   //calculate average value of r and g and b values (assign to a float variable)
+	   //round float variable to integer (add 0.5 before casting to integer)
+	   //assign the rounded value to r,g and b values
+	for(int y=0;y<p->height;y++){
+	 for(int x=0;x<p->width;x++){
+			const rgba pixel=p->pixArray[y][x]; 
+			const float average= (pixel.r+pixel.g+pixel.b)/3.0f;
+			const unsigned char iaverage=average+.5;
+			p->pixArray[y][x].r=iaverage;
+		 p->pixArray[y][x].g=iaverage;			
+		 p->pixArray[y][x].b=iaverage;
+		}	
+	}
+	//pixMap temp = pixMap_init;
+	//pixMap_copy(temp, p);
+	//pixMap_copy(p, temp);
+	return;
 }
 int pixMap_write(pixMap *p,char *filename){
- //write out to filename using lodepng_encode32_file
- //example is in lodepng/examples - one liner
+	int error=0;
+ if(lodepng_encode32_file(filename, p->image, p->width, p->height)){
+  fprintf(stderr,"error %u: %s\n", error, lodepng_error_text(error));
+  return 1;
+	}
+	
 	return 0;
-}	 
+}	
+
+static int pixMap_cmp(const void *a, const void *b) {
+	
+	const rgba *ra = (rgba*) a;
+	const rgba *rb = (rgba*) b;
+	return((ra->r+ra->g+ra->b) - (rb->r+rb->g+rb->b));
+}
+
+void pixMap_sort(pixMap *p) {
+	
+	qsort(p->image, p->width*p->height, sizeof(rgba),pixMap_cmp);
+	
+	
+}
+
+
+
+ BMP16_map* pixMap_32_to_16(pixMap *p){
+	 
+	 BMP16_map *p16 = BMP16_map_init(p->height, p->width, 0, 5, 6, 5);
+	 //loop through the array of arrays
+	 uint16_t bit16;
+	 for(int i = 0; i< p->height; i++) {
+		 for(int j =0; j< p->width; j++) {			 
+			 uint16_t r = p->pixArray[i][j].r >> 3;
+			 uint16_t g = p->pixArray[i][j].g >> 2;
+			 uint16_t b = p->pixArray[i][j].b >> 3;
+			 //fprintf(stdout, "Red: %d\n Green: %d\n Blue: %d\n", p16->Rbits, p16->Gbits, p16->Bbits );			 
+			 //printf("PRINTING BIT16");
+			 bit16 = ((r) <<11) | ((g) << 5)| (b);			 
+			 p16->pixArray[(p->height-1) - i][j] = bit16;
+			 
+		 }
+		 
+	
+	 }
+	 
+	 
+ 
+ return p16;
+}
--- pngPlay.c
+++ pngPlay.c
@@ -1,12 +1,68 @@
 #include "pixutils.h"
+#include "bmp.h"
 
 int main(int argc, char *argv[]){
   char *inputfile=0,*outputfile=0;
-  float degrees=0, grayFlag=0;
+  float degrees=0, grayFlag=0, sort=0, save=0;
   
   //write the parser yourself or use a package like https://github.com/skeeto/optparse
-  
-		//check for flags -i -o -r -g - can be in any order
+
+
+int i;
+for(i=0; i<argc; i++){
+	fprintf(stderr, "argv[%d] = %s\n", i, argv[i]);
+}
+
+	if(argc>9||argc<3) {
+		fprintf(stderr, "Not the right number of argument\n");
+		//printf(stdout, "Number of args: %d",argc);
+		return(1);
+	}
+
+	int j=1;
+
+	while(j<argc){
+		if(strcmp(argv[j], "-i") ==0){
+			inputfile=argv[j+1];
+			fprintf(stderr, "the input file is %s\n" , inputfile);
+			j+=2;
+		}
+		else if (strcmp(argv[j],"-o") ==0) {
+			outputfile=argv[j+1];
+			fprintf(stderr, "the output file is %s\n" , outputfile);
+			j+=2;
+
+
+		}
+		else if(strcmp(argv[j], "-r")==0) {
+			degrees=atof(argv[j+1]);
+			fprintf(stderr, "the degree is %f\n" , degrees);
+			j+=2;
+		}
+		else if(strcmp(argv[j], "-g")==0) {
+			grayFlag=1;
+			fprintf(stderr, "Gray Scale true\n");
+			//fprintf(stderr, "the gray scale is %f\n" , grayFlag);
+			j+=1;
+		}
+		
+		else if(strcmp(argv[j], "-b")==0) {
+			
+			save=1;
+			fprintf(stderr, "save 16 bits true\n");
+			j+=1;
+			
+			//save as 16 bit
+		}
+		else if(strcmp(argv[j] , "-s")==0) {
+			sort=1;
+			fprintf(stderr, "Sort is true\n");
+			j+=1;
+			//sort pix
+		}
+
+	}
+  //check for flags -i -o -r -g - can be in any order
   //-i is followed by the input png
   //-o is followed the output png
   //-r is followd by the rotation angle in degrees (float) <optional for user>
@@ -15,9 +71,16 @@ int main(int argc, char *argv[]){
   pixMap *p=pixMap_init_filename(inputfile);
   if(degrees)pixMap_rotate(p,degrees);
   if(grayFlag)pixMap_gray(p);
-  pixMap_write(p,outputfile);
-  pixMap_destroy(p);
-  
+  if(sort)pixMap_sort(p);
+  if(save) {
+	  BMP16_map *pix = pixMap_32_to_16(p);
+	  
+	  BMP16_write(pix, outputfile);
+	  BMP16_map_destroy(pix);
+	  } else  pixMap_write(p,outputfile);
+	pixMap_destroy(p);
+	
+   
   return 0;
 }
 
